e19e81c095499a0211c12c19a82635b2c381613d
issu478 <issumr860@gmail.com>
2025-09-24 13:00:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,11 +5,12 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "jlJxRJiB#qXTB_EVy7O_GOXCBCHRzlDy81OCmXFRG2Laj2mpEXJU",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/4bc81k.png",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 QUEEN-MAYA-MD Is Alive Now�*\n\TYPE . AND menu TO BEGIN.",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello there, ‘👑 QUEEN-MAYA-MD Is Alive NowðŸ*\n\TYPE . AND menu TO BEGIN.",
 BOT_OWNER: '94716717099',  // Replace with the owner's phone number
 
 
 
 };
+
